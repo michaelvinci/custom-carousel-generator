dbef49e7d680e7722427520563ce50da3ef462ea
Michael <michaeldanvinci@gmail.com>
2019-06-07 23:01:03 -0700

    added app scaffolding and dependencies

---
+++ src/components/App/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App__header">
+        Custom Image Carousel Generator
+      </header>
+    </div>
+  );
+}
+
+export default App;
