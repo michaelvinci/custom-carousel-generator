3fb7371e7be56f0320d0ab3684fd118bdf9e24f2
Michael <michaeldanvinci@gmail.com>
2019-06-22 22:36:10 -0700

    Add docs folder for gh-pages deployment

---
+++ docs/precache-manifest.53e1e9c2c6183679ecaef154be90be81.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c99b2851eca34834f2e2815cb4aac20",
+    "url": "/carousel-test/index.html"
+  },
+  {
+    "revision": "39a6d990cc28650d3595",
+    "url": "/carousel-test/static/css/main.186d8f59.chunk.css"
+  },
+  {
+    "revision": "acab08e6f019f89251b9",
+    "url": "/carousel-test/static/js/2.fffffdbe.chunk.js"
+  },
+  {
+    "revision": "39a6d990cc28650d3595",
+    "url": "/carousel-test/static/js/main.d0179313.chunk.js"
+  },
+  {
+    "revision": "55004613d1f94783b713",
+    "url": "/carousel-test/static/js/runtime~main.ea77ea90.js"
+  },
+  {
+    "revision": "de44ac7b6a3c187b22836d7ce3440534",
+    "url": "/carousel-test/static/media/brushed_metal_dark_med_hd.de44ac7b.jpg"
+  }
+]);
