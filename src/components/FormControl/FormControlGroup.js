f71a661939a71400258dd1d15df905ebc21e27a5
Michael <michaeldanvinci@gmail.com>
2019-06-22 23:32:40 -0700

    Fix UI style inconsistencies

--- src/components/FormControl/FormControlGroup.js
+++ src/components/FormControl/FormControlGroup.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const FormControlGroup = (props) => {
   const styles = props.isDisabled ? 'input-group disabled' : 'input-group';
-  const removed = props.isRemoved ? 'removed' : null;
+  const removed = props.isRemoved ? 'removed' : '';
 
   return (
     <div className={`${styles} ${removed}`}>
